32b07317fe9ad1355e4f3a1c01b3dfc8b43ead3d
rickNoise <emberix@gmail.com>
2025-05-20 10:40:56 +1000

    thru CH2 L4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -45,7 +45,38 @@ class TestHtmlNode(unittest.TestCase):
         node = HTMLNode("a", "link_value", None, props)
         expected_result = "HTMLNode(tag=a, value=link_value, children=None, props={'href': 'https://www.google.com', 'target': '_blank'})"
         self.assertEqual(node.__repr__(), expected_result)
-        
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_node_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(
+            node.to_html(),
+            "<p>This is a paragraph of text.</p>"
+        )
+    
+    def test_leaf_node_to_html_2(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+    
+    def test_leaf_node_to_html_3(self):
+        node = LeafNode(None, "Hello world!")
+        self.assertEqual(
+            node.to_html(),
+            "Hello world!"
+        )
+    
+    def test_leaf_node_repr(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.__repr__(),
+            "LeafNode(a, Click me!, {'href': 'https://www.google.com'})"
+        )
+ 
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,7 +3,7 @@ import unittest
 from textnode import TextNode, TextType
 
 
-class TestTestText(unittest.TestCase):
+class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
