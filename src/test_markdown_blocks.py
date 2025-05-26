d1686c3063d4615b20ea070aa5f29dc36cef9528
rickNoise <emberix@gmail.com>
2025-05-26 14:05:04 +1000

    added extra test to test_markdown_blocks.py

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -3,10 +3,6 @@ import unittest
 from markdown_blocks import (
     markdown_to_blocks
 ) 
-from textnode import (
-    TextNode, 
-    TextType, 
-)
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -30,3 +26,25 @@ This is the same paragraph on a new line
             ],
         )
        
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
