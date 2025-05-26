95202ce7b24102b6c638240030982fed6bb0ae6f
rickNoise <emberix@gmail.com>
2025-05-26 14:31:31 +1000

    thru CH4 L2

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,53 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    OLIST = "ordered_list"
+    ULIST = "unordered_list"
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith((
+        "# ",
+        "## ",
+        "### ",
+        "#### ",
+        "##### ",
+        "###### ",
+    )):
+        return BlockType.HEADING
+    if (
+        len(lines) > 1 
+        and lines[0].startswith("```")
+        and lines[-1].startswith("```")
+    ):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        current_number = 1
+        for line in lines:
+            if not line.startswith(f"{str(current_number) + ". "}"):
+                return BlockType.PARAGRAPH
+            current_number += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
+
+
 def markdown_to_blocks(markdown):
     pre_stripped_blocks = markdown.split("\n\n")
     stripped_blocks = []
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,9 +1,46 @@
 import unittest
 
 from markdown_blocks import (
-    markdown_to_blocks
-) 
+    markdown_to_blocks,
+    BlockType,
+    block_to_block_type,
+)
 
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        md = "### Heading 3"
+        er = BlockType.HEADING
+        self.assertEqual(block_to_block_type(md), er)
+    
+    def test_code(self):
+        md = "```\nthis is a \ncode block\n```"
+        er = BlockType.CODE
+        self.assertEqual(block_to_block_type(md), er)
+    
+    def test_quote(self):
+        md = ">this is a quote\n> block\n>for testing"
+        er = BlockType.QUOTE
+        self.assertEqual(block_to_block_type(md), er)
+
+    def test_ulist(self):
+        md = "- This is a\n- unordered list\n- with three lines"
+        er = BlockType.ULIST
+        self.assertEqual(block_to_block_type(md), er)
+    
+    def test_olist(self):
+        md = """1. one
+2. two
+3. three
+4. four
+5. five
+6. six
+7. seven
+8. eight
+9. nine
+10. ten"""
+        er = BlockType.OLIST
+        self.assertEqual(block_to_block_type(md), er) 
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
