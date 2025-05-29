b45ce6172be469ca4f87536ddf9b311865842b42
rickNoise <emberix@gmail.com>
2025-05-29 13:01:56 +1000

    thru CH5 L4

--- src/generate_page.py
+++ src/generate_page.py
@@ -36,3 +36,58 @@ def generate_page(from_path, template_path, dest_path):
     
     if not f.closed:
         raise Exception("dest_path file did not close successfully!")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, dest_file_ext=".html"):
+    """
+    Crawl every entry in the content directory.
+    For each markdown file found, generate a new .html file using the same template.html. 
+    The generated pages should be written to the public directory in the 
+    same directory structure.
+    """
+
+    # check that dir_path_content exists and is a directory
+    if not os.path.exists(dir_path_content):
+        raise ValueError("Provided dir_path_content does not exist!")
+    if not os.path.isdir(dir_path_content):
+        raise ValueError("Provided dir_path_content is not a directory!")
+    
+    print(f"Recursively generating pages from {dir_path_content} to {dest_dir_path} using {template_path}...")
+
+    # crawl every entry in the content directory
+    print(f"Crawling all entries in {dir_path_content}...")
+    for entry in os.listdir(dir_path_content):
+        entry_path = os.path.join(dir_path_content, entry)
+        print(f"processing entry {entry} at path {entry_path}")
+        if os.path.isfile(entry_path):
+            file_name, file_extension = os.path.splitext(entry)
+            if file_extension == ".md":
+                print(f"{entry} is a markdown file. Running generate_page using template path {template_path}...")
+                generate_page(
+                    entry_path, 
+                    template_path, 
+                    os.path.join(dest_dir_path, file_name + dest_file_ext)
+                )
+        if os.path.isdir(entry_path):
+            print(f"{entry} is a directory. Recursively running generate_pages_recursive with dir_path_content: {dir_path_content}, template_path: {os.path.abspath(template_path)}, and dest_dir_path: {dest_dir_path}...")
+
+            # create the directory first if it doesn't exist
+            new_dest_dir_path = os.path.join(dest_dir_path, entry)
+            if not os.path.exists(new_dest_dir_path):
+                try:
+                    os.mkdir(new_dest_dir_path)
+                    print(f"Directory '{new_dest_dir_path}' created successfully.")
+                except PermissionError:
+                    print(f"Permission denied: Unable to create '{new_dest_dir_path}'.")
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+
+            generate_pages_recursive(
+                os.path.join(dir_path_content, entry),
+                os.path.abspath(template_path),
+                new_dest_dir_path
+            )
+    
+    print("COMPLETED running generate_pages_recursive.")
+
+
--- src/main.py
+++ src/main.py
@@ -2,24 +2,32 @@ import os, shutil
 from textnode import TextType, TextNode
 
 from copystatic import copy_directory_contents
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
 generate_page_from_path = "./content/index.md"
 generate_page_template_path = "./template.html"
 generate_page_dest_path = "./public/index.html"
-
+generate_pages_recursive_dir_path_content = "./content"
+generate_pages_recursive_template_path = "./template.html"
+generate_pages_recursive_dest_dir_path = "./public"
 
 def main():
     print("Deleting public directory...")
     print("Copying static files to public directory...")
+    # note that this function does the deleting AND the copying
     copy_directory_contents(dir_path_static, dir_path_public)
 
-    generate_page(
-        generate_page_from_path, 
-        generate_page_template_path, 
-        generate_page_dest_path
+    # generate_page(
+    #     generate_page_from_path, 
+    #     generate_page_template_path, 
+    #     generate_page_dest_path
+    # )
+    generate_pages_recursive(
+        generate_pages_recursive_dir_path_content,
+        generate_pages_recursive_template_path,
+        generate_pages_recursive_dest_dir_path
     )
 
 
